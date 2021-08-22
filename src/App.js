f0fb8adad0ab664f1839b92cba307e633f242b73
Hani <hani@Hanis-MacBook-Pro.local>
2021-08-22 01:06:25 +0100

    added double click reminder func

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Tasks from "./components/Tasks";
 
 
 const App = () => {
-  const [tasks, setstate] = useState([    
+  const [tasks, setTasks] = useState([    
     {
         id: 1,
         text: 'Doc Appt',
         day: 'Feb 5th at 2:30PM',
-        reminder: false
+        reminder: true
      },
      {
          id: 2,
@@ -24,10 +24,18 @@ const App = () => {
          reminder: false
       }
     ])
+
+    const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    const toggleReminder = (id) => {
+      setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+    }
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No tasks to show')}
     </div>
   );
 }
