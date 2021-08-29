9a8c7d5052ff1476842622f8d04acd5e3e2f781d
Hani <h.samir@edu.salford.ac.uk>
2021-08-29 19:50:52 +0100

    Add task functioanlity

--- src/App.js
+++ src/App.js
@@ -25,18 +25,27 @@ const App = () => {
          reminder: false
       }
     ])
-
+    //delete task
     const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id !== id))
     }
-
+    // toggle reminder
     const toggleReminder = (id) => {
       setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+    }
+
+    // add task
+
+    const addTask = (task) => {
+      const id = Math.floor(Math.random() * 1000) + 1;
+      const newTask = {id, ...task}
+
+      setTasks([...tasks, newTask]);
     }
   return (
     <div className="container">
       <Header/>
-      <AddTask/>
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No tasks to show')}
     </div>
   );
